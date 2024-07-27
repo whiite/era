ada1ae687322a65a954d6e262aed6009ffb2cdfa
Liam Moore <liamail4@gmail.com>
2024-07-27 18:58:51 +0100

    feat: support supplying canonical time zones

--- cmd/now.go
+++ cmd/now.go
@@ -7,10 +7,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Format string flag to output the time with
 var Format string
+
+// Canonical time zone string flag to convert the time to before displaying
+var TimeZone string
 
 func init() {
 	nowCmd.Flags().StringVarP(&Format, "format", "f", "", "Format to display the datetime with")
+	nowCmd.Flags().StringVarP(&TimeZone, "timezone", "t", "", "Time zone to set the time to")
 	rootCmd.AddCommand(nowCmd)
 }
 
@@ -18,8 +23,17 @@ var nowCmd = &cobra.Command{
 	Use:   "now",
 	Short: "Get and manipulate the current time",
 	Long:  "Convert, format and print the current time ",
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		now := time.Now()
+
+		if TimeZone != "" {
+			location, err := time.LoadLocation(TimeZone)
+			if err != nil {
+				return err
+			}
+			now = now.In(location)
+		}
+
 		switch Format {
 		case "unix", "timestamp", "ts":
 			fmt.Println(now.Unix())
@@ -27,6 +41,9 @@ var nowCmd = &cobra.Command{
 			fmt.Println(now)
 		default:
 			fmt.Println(now.Format(Format))
+			return fmt.Errorf("Invalid format '%s'", Format)
 		}
+
+		return nil
 	},
 }
