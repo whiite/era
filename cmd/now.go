f8d31f9aef4b7a3afefc226268dc1b017772ce9a
Liam Moore <liamail4@gmail.com>
2024-07-30 21:27:22 +0100

    feat: initial support for custom format strings based on the date CLI tokens

--- cmd/now.go
+++ cmd/now.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 
@@ -45,10 +46,63 @@ var nowCmd = &cobra.Command{
 		case "go", "":
 			fmt.Println(now)
 		default:
-			fmt.Println(now.Format(Format))
-			return fmt.Errorf("Invalid format '%s'", Format)
+			fmt.Println(parseFormatLinux(now, &Format))
 		}
 
 		return nil
 	},
+}
+
+func parseFormatLinux(date time.Time, formatString *string) string {
+	tokenMap := map[rune]func(dt time.Time) string{
+		'Y': func(dt time.Time) string { return strconv.Itoa(dt.Year()) },
+		'm': func(dt time.Time) string { return strconv.Itoa(int(dt.Month())) },
+		'B': func(dt time.Time) string { return dt.Month().String() },
+		'b': func(dt time.Time) string { return dt.Month().String()[:3] },
+		'd': func(dt time.Time) string { return strconv.Itoa(dt.Day()) },
+		'j': func(dt time.Time) string { return strconv.Itoa(dt.YearDay()) },
+		'u': func(dt time.Time) string { return strconv.Itoa(int(dt.Weekday())) },
+		'A': func(dt time.Time) string { return dt.Weekday().String() },
+		'a': func(dt time.Time) string { return dt.Weekday().String()[:3] },
+		'H': func(dt time.Time) string { return strconv.Itoa(dt.Hour()) },
+		'I': func(dt time.Time) string { return strconv.Itoa(dt.Hour() % 12) },
+		'M': func(dt time.Time) string { return strconv.Itoa(dt.Minute()) },
+		'S': func(dt time.Time) string { return strconv.Itoa(dt.Second()) },
+		'n': func(dt time.Time) string { return "\n" },
+		'R': func(dt time.Time) string { return strconv.Itoa(dt.Hour()) + ":" + strconv.Itoa(dt.Minute()) },
+		'T': func(dt time.Time) string {
+			return strconv.Itoa(dt.Hour()) + ":" + strconv.Itoa(dt.Minute()) + ":" + strconv.Itoa(dt.Second())
+		},
+		'U': func(dt time.Time) string {
+			_, weekNumber := dt.ISOWeek()
+			return strconv.Itoa(weekNumber)
+		},
+	}
+
+	var formattedDate strings.Builder
+	interpretToken := false
+	substitutePrefix := '%'
+
+	for _, char := range *formatString {
+		if char == substitutePrefix && interpretToken == false {
+			interpretToken = true
+			continue
+		}
+
+		formatFunc := tokenMap[char]
+		if interpretToken && formatFunc != nil {
+			formattedDate.WriteString(formatFunc(date))
+			interpretToken = false
+			continue
+		}
+
+		if interpretToken {
+			formattedDate.WriteRune(substitutePrefix)
+		}
+
+		formattedDate.WriteRune(char)
+		interpretToken = false
+	}
+
+	return formattedDate.String()
 }
