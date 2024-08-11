fe31a1336fdb5b54c90b6b5b523036f98b967696
Liam Moore <liamail4@gmail.com>
2024-08-11 10:31:05 +0100

    feat: initial support for querying tokens

---
+++ cmd/tokens.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"monokuro/era/parser"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	tokensCmd.Flags().StringVarP(&Parser, "parser", "p", "", "Parser to interpret supplied time wFormat to display the datetime withith")
+	rootCmd.AddCommand(tokensCmd)
+}
+
+var tokensCmd = &cobra.Command{
+	Use:   "tokens",
+	Short: "Token info for parsers",
+	Long:  "Get and query tokens for specified parsers to see what they map to",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		switch Parser {
+		case "moment", "momentjs":
+			if len(args) == 0 {
+				return fmt.Errorf("Showing all tokens not supported yet. Please specify at least one token")
+			}
+			fmt.Println("Unsupported currently")
+		case "strptime":
+			if len(args) == 0 {
+				return fmt.Errorf("Showing all tokens not supported yet. Please specify at least one token")
+			}
+			tokenChar := []rune(args[0])[0]
+			token, hasToken := parser.Strptime.TokenMap[tokenChar]
+			if !hasToken {
+				return fmt.Errorf("Token '%c' is not supported for this parser", tokenChar)
+			}
+			fmt.Printf("'%c%c': %s\n", parser.Strptime.Prefix, tokenChar, token.Desc)
+		default:
+			return fmt.Errorf("No parser specified")
+		}
+		return nil
+	},
+}
