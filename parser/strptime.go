b2de75635756bd57a175054e26d71a103ffee3ca
White <liamail4@gmail.com>
2024-12-14 11:18:35 +0000

    feat: add remaining single character tokens to the strptime parser

--- parser/strptime.go
+++ parser/strptime.go
@@ -49,6 +49,24 @@ var Strptime = DateFormatterPrefix{
 			Desc:   "Day of month (1-31)",
 			expand: func(dt time.Time, locale locales.Translator) string { return fmt.Sprintf("% 2d", dt.Day()) },
 		},
+		'F': {
+			Desc:   "Date in year-month-day format equivalent to '%Y-%m-%d' - '2024-01-04', '1997-10-31'",
+			expand: func(dt time.Time, locale locales.Translator) string { return fmt.Sprintf("% 2d", dt.Day()) },
+		},
+		'g': {
+			Desc: "ISO week year shortened to the last two digits (00-99) ",
+			expand: func(dt time.Time, locale locales.Translator) string {
+				year, _ := dt.ISOWeek()
+				return fmt.Sprintf("%02d", year%100)
+			},
+		},
+		'G': {
+			Desc: "ISO week year - '1999', '2007'",
+			expand: func(dt time.Time, locale locales.Translator) string {
+				year, _ := dt.ISOWeek()
+				return fmt.Sprintf("%d", year)
+			},
+		},
 		'D': {
 			Desc: "American style date (month first) equivalent to '%m/%d/%y' where the year is truncated to the last two digits - '01/31/97', '02/28/01'",
 			expand: func(dt time.Time, locale locales.Translator) string {
@@ -60,12 +78,41 @@ var Strptime = DateFormatterPrefix{
 			expand: func(dt time.Time, locale locales.Translator) string { return fmt.Sprintf("%02d", dt.Hour()) },
 		},
 		'I': {
-			Desc:   "Hour in 12 hour format zero padded to two digits (01-12)",
-			expand: func(dt time.Time, locale locales.Translator) string { return fmt.Sprintf("%02d", dt.Hour()%12) },
+			Desc: "Hour in 12 hour format zero padded to two digits (01-12)",
+			expand: func(dt time.Time, locale locales.Translator) string {
+				hour := dt.Hour() % 12
+				if hour == 0 {
+					hour = 12
+				}
+				return fmt.Sprintf("%02d", hour)
+			},
 		},
 		'j': {
 			Desc:   "Day of year zero padded to three digits (001-366)",
 			expand: func(dt time.Time, locale locales.Translator) string { return fmt.Sprintf("%03d", dt.YearDay()) },
+		},
+		'k': {
+			Desc: "Hour in 24 hour format space padded to two digits ( 0-23)",
+			expand: func(dt time.Time, locale locales.Translator) string {
+				hour := dt.Hour()
+				if hour > 9 {
+					return fmt.Sprintf("%2d", hour)
+				}
+				return fmt.Sprintf(" %d", hour)
+			},
+		},
+		'l': {
+			Desc: "Hour in 12 hour format space padded to two digits ( 0-12)",
+			expand: func(dt time.Time, locale locales.Translator) string {
+				hour := dt.Hour() % 12
+				if hour == 0 {
+					hour = 12
+				}
+				if hour > 9 {
+					return fmt.Sprintf("%2d", hour)
+				}
+				return fmt.Sprintf(" %d", hour)
+			},
 		},
 		'm': {
 			Desc:   "Month number zero padded to two digits (01-12)",
@@ -78,6 +125,10 @@ var Strptime = DateFormatterPrefix{
 		'n': {
 			Desc:   "Newline whitespace - '\\n'",
 			expand: func(dt time.Time, locale locales.Translator) string { return "\n" },
+		},
+		'N': {
+			Desc:   "Nanoseconds zero padded to nine digits (000000000-999999999)",
+			expand: func(dt time.Time, locale locales.Translator) string { return strconv.Itoa(dt.Nanosecond()) },
 		},
 		'p': {
 			Desc: "The locale's equivalent of AM or PM (hardcoded to English am/pm)",
@@ -92,11 +143,16 @@ var Strptime = DateFormatterPrefix{
 		'r': {
 			Desc: "12 hour time represented as hours, minutes, seconds and am/pm equivalent to \"%I:%M:%S %p\" (hardcoded to English am/pm) - '11:24:52 pm', '04:09:20 am'",
 			expand: func(dt time.Time, locale locales.Translator) string {
+				hour := dt.Hour() % 12
+				if hour == 0 {
+					hour = 12
+				}
+
 				ampm := "pm"
 				if dt.Hour() < 12 {
 					ampm = "am"
 				}
-				return fmt.Sprintf("%02d:%02d:%02d %s", dt.Hour()%12, dt.Minute(), dt.Second(), ampm)
+				return fmt.Sprintf("%02d:%02d:%02d %s", hour, dt.Minute(), dt.Second(), ampm)
 			},
 		},
 		'R': {
@@ -119,12 +175,25 @@ var Strptime = DateFormatterPrefix{
 				return fmt.Sprintf("%02d:%02d:%02d", dt.Hour(), dt.Minute(), dt.Second())
 			},
 		},
+		'u': {
+			Desc: "Day of week where Monday = 1 and Sunday = 7 (1-7)",
+			expand: func(dt time.Time, locale locales.Translator) string {
+				return strconv.Itoa((7+int(dt.Weekday()))%7 + 1)
+			},
+		},
 		// TODO: implement same rules as strptime - first week is the first Sunday of January
 		'U': {
 			Desc: "ISO8601 week number of the year (this is different to the typical strptime token currently)",
 			expand: func(dt time.Time, locale locales.Translator) string {
 				_, weekNumber := dt.ISOWeek()
 				return fmt.Sprintf("%02d", weekNumber)
+			},
+		},
+		'V': {
+			Desc: "ISO8601 week number of the year zero padded to two digits - (01-53)",
+			expand: func(dt time.Time, locale locales.Translator) string {
+				_, week := dt.ISOWeek()
+				return fmt.Sprintf("%02d", week)
 			},
 		},
 		'w': {
@@ -158,6 +227,27 @@ var Strptime = DateFormatterPrefix{
 		'Y': {
 			Desc:   "Year number - '1999', '2007'",
 			expand: func(dt time.Time, locale locales.Translator) string { return strconv.Itoa(dt.Year()) },
+		},
+		'z': {
+			Desc: "Time zone offset in +hhmm format - '-0400', '+0530'",
+			expand: func(dt time.Time, locale locales.Translator) string {
+				_, offsetSeconds := dt.Zone()
+				offsetMinutes := offsetSeconds / 60
+				offsetHours := offsetMinutes / 60
+				sign := '+'
+				if offsetHours < 0 {
+					sign = '-'
+				}
+				return fmt.Sprintf("%c%02d%02d", sign, offsetHours, offsetMinutes%60)
+
+			},
+		},
+		'Z': {
+			Desc: "Abbreviated time zone offset - 'GMT', 'CEST', '+0530'",
+			expand: func(dt time.Time, locale locales.Translator) string {
+				offsetName, _ := dt.Zone()
+				return offsetName
+			},
 		},
 	},
 }
